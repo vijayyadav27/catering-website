33ce13c3c31f3f899fab2618ba255f02984fc0b3
vijayyadav27 <vijay2792004@gmail.com>
2025-11-27 22:05:27 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+    apiKey: "AIzaSyADU5yKQyf2EuMxZX8WWMrncbp6-Rmmnww",
+    authDomain: "catering-website-41067.firebaseapp.com",
+    projectId: "catering-website-41067",
+    storageBucket: "catering-website-41067.firebasestorage.app",
+    messagingSenderId: "548514111244",
+    appId: "1:548514111244:web:620a94e5c4a05f51667329"
 };
 
 firebase.initializeApp(firebaseConfig);
